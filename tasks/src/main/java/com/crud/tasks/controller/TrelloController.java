d881ed601ad9fc194799d3d30da5888eb0dcc518
Vol-midora <j.j.palinski@gmail.com>
2018-07-16 10:58:52 +0200

    zadanie 24.3

---
+++ tasks/src/main/java/com/crud/tasks/controller/TrelloController.java
@@ -0,0 +1,30 @@
+package com.crud.tasks.controller;
+
+import com.crud.tasks.domain.CreatedTrelloCard;
+import com.crud.tasks.domain.TrelloBoardDto;
+import com.crud.tasks.domain.TrelloCardDto;
+import com.crud.tasks.service.TrelloService;
+import com.crud.tasks.trello.client.TrelloClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/v1/trello")
+@CrossOrigin("*")
+public class TrelloController {
+
+    @Autowired
+    private TrelloService trelloService;
+
+    @RequestMapping(method = RequestMethod.GET, value = "/boards")
+    public List<TrelloBoardDto> getTrelloBoards() {
+        return trelloService.fetchTrelloBoards();
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = "/cards")
+    public CreatedTrelloCard createTrelloCard(@RequestBody TrelloCardDto trelloCardDto) {
+        return trelloService.createdTrelloCard(trelloCardDto);
+    }
+}
---
+++ tasks/src/main/java/com/crud/tasks/service/SimpleEmailService.java
@@ -0,0 +1,65 @@
+package com.crud.tasks.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.MailException;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.mail.javamail.MimeMessagePreparator;
+import org.springframework.stereotype.Service;
+
+import com.crud.tasks.domain.Mail;
+import com.crud.tasks.scheduler.EmailScheduler;
+
+@Service
+public class SimpleEmailService {
+	private static final Logger LOGGER = LoggerFactory.getLogger(SimpleMailMessage.class);
+
+	@Autowired
+	private JavaMailSender javaMailSender;
+
+	@Autowired
+	private MailCreatorService mailCreatorService;
+
+	public void send(final Mail mail) {
+		LOGGER.info("Starting e-mail preparation");
+
+		try {
+			// SimpleMailMessage mailMessage = createMailMessage(mail);
+			// javaMailSender.send(mailMessage);
+			javaMailSender.send(createMimeMessage(mail));
+			LOGGER.info("E-mail sent");
+		} catch (MailException e) {
+			LOGGER.error("Failed to process the e-mail sending: ", e.getMessage(), e);
+		}
+	}
+
+	private MimeMessagePreparator createMimeMessage (final Mail mail){
+		return mimeMessage -> {
+			MimeMessageHelper messageHelper = new MimeMessageHelper(mimeMessage);
+			messageHelper.setTo(mail.getMailTo());
+			messageHelper.setSubject(mail.getSubject());
+			if(mail.getSubject().equals(TrelloService.SUBJECT)) {
+				messageHelper.setText(mailCreatorService.buildTrelloCardEmail
+						(mail.getMessage()), true);
+			} else if (mail.getSubject().equals(EmailScheduler.SUBJECT)) {
+				messageHelper.setText(mailCreatorService.buildTaskAmountEmail(mail.getMessage()), true);
+			} else {
+				messageHelper.setText(mail.getMessage(), false);
+			}
+		};
+	}
+
+	private SimpleMailMessage createMailMessage(final Mail mail) {
+		SimpleMailMessage mailMessage = new SimpleMailMessage();
+		mailMessage.setTo(mail.getMailTo());
+		mailMessage.setSubject(mail.getSubject());
+		mailMessage.setText(mail.getMessage());
+		if (mail.getToCc() != null) {
+			mailMessage.setCc(mail.getToCc());
+		}
+		return mailMessage;
+	}
+}
